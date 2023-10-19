e1c537ead3da386a0ac8ac022a37407f3bed46c6
Ederson Torresini <boidacarapreta@gmail.com>
2023-10-19 03:52:12 +0000

    Adicionar /credito

--- servidor.js
+++ servidor.js
@@ -8,8 +8,9 @@ const port = process.env.PORT || 3000
 const pool = new Pool() // usadas as variáveis de ambiente conforme https://www.postgresql.org/docs/9.1/libpq-envars.html
 
 app.use(bodyParser.json())
+
 const extratoEsquema = Joi.object({
-  id: Joi.number().integer().min(0),
+  id: Joi.number().integer().min(1),
   senha: Joi.number().integer().min(0)
 })
 
@@ -23,11 +24,11 @@ app.get('/api/v1/extrato', async (req, res) => {
   try {
     const auth = await pool.query('SELECT id FROM jogadores WHERE id = $1 AND senha = $2', [req.body.id, req.body.senha])
     if (auth.rowCount === 0) {
-      res.status(401).send()
+      res.status(401).send('Unauthorized')
       return
     }
 
-    let receitas = await pool.query('SELECT jogos.nome AS jogo, to_char(receitas.data, \'DD/MM/YYYY HH24:MI:SS\') AS data, receitas.valor FROM receitas INNER JOIN jogos ON jogos.id = receitas.jogo_id WHERE receitas.jogador_id = (SELECT id FROM jogadores WHERE id = $1 AND senha = $2)', [req.body.id, req.body.senha])
+    let receitas = await pool.query('SELECT jogos.nome AS jogo, to_char(receitas.data, \'DD/MM/YYYY HH24:MI:SS\') AS data, receitas.valor FROM receitas INNER JOIN jogos ON jogos.id = receitas.jogo_id WHERE receitas.jogador_id = $1', [req.body.id])
     receitas = { receitas: receitas.rows }
 
     /*
@@ -35,7 +36,7 @@ app.get('/api/v1/extrato', async (req, res) => {
     response = response.concat(receitasTotal.rows)
     */
 
-    let despesas = await pool.query('SELECT produtos.descricao AS produto, to_char(despesas.data, \'DD/MM/YYYY HH24:MI:SS\') AS data, despesas.valor FROM despesas INNER JOIN produtos ON produtos.id = despesas.produto_id WHERE despesas.jogador_id = (SELECT id FROM jogadores WHERE id = $1 AND senha = $2)', [req.body.id, req.body.senha])
+    let despesas = await pool.query('SELECT produtos.descricao AS produto, to_char(despesas.data, \'DD/MM/YYYY HH24:MI:SS\') AS data, despesas.valor FROM despesas INNER JOIN produtos ON produtos.id = despesas.produto_id WHERE despesas.jogador_id = $1', [req.body.id])
     despesas = { despesas: despesas.rows }
 
     /*
@@ -45,7 +46,36 @@ app.get('/api/v1/extrato', async (req, res) => {
 
     res.json({ ...receitas, ...despesas })
   } catch (err) {
-    res.status(500).send()
+    res.status(500).send('Database error')
+  }
+})
+
+const creditoEsquema = Joi.object({
+  id: Joi.number().integer().min(1),
+  senha: Joi.number().integer().min(0),
+  jogo: Joi.number().integer().min(1),
+  valor: Joi.number().integer().min(1)
+})
+
+app.get('/api/v1/credito', async (req, res) => {
+  const validationResult = creditoEsquema.validate(req.body)
+  if (validationResult.error) {
+    res.status(400).send(validationResult.error.details[0].message)
+    return
+  }
+
+  try {
+    const auth = await pool.query('SELECT id FROM jogadores WHERE id = $1 AND senha = $2', [req.body.id, req.body.senha])
+    if (auth.rowCount === 0) {
+      res.status(401).send('Unauthorized')
+      return
+    }
+
+    const credito = await pool.query('INSERT INTO receitas (jogador_id, jogo_id, valor, data) values ($1, $2, $3, NOW())', [req.body.id, req.body.jogo, req.body.valor])
+
+    res.json(credito)
+  } catch (err) {
+    res.status(500).send('Database error')
   }
 })
 
