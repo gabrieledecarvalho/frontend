da462972c5b331919b69ab3bff69bc496ea1356c
leojungneves <leojn132013@gmail.com>
2023-10-26 10:47:10 -0300

    Post estoque de maquinas

--- servidor.js
+++ servidor.js
@@ -5,8 +5,10 @@ const port = process.env.PORT || 3000
 const routeExtrato = require('./routes/extrato')
 const routeCredito = require('./routes/credito')
 const routeDebito = require('./routes/debito')
+const routepostEstoque = require('./routes/postEstoque')
 
 app.use('/api/v1', routeExtrato)
 app.use('/api/v1', routeCredito)
 app.use('/api/v1', routeDebito)
+app.use('/api/v1', routepostEstoque)
 app.listen(port, () => { console.log(`Server running at http://localhost:${port}/`) })
