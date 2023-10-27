799c9db9bec217bfce73db856dc351d6213d3257
leojungneves <leojn132013@gmail.com>
2023-10-26 23:51:46 -0300

    adicionando arquivos frontEnd

---
+++ FrontEnd/script.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+const loginForm = document.getElementById('loginForm')
+const usernameInput = document.getElementById('username')
+const passwordInput = document.getElementById('password')
+
+loginForm.addEventListener('submit', function (event) {
+  event.preventDefault()
+
+  const username = usernameInput.value
+  const password = passwordInput.value
+
+  axios.post('https://feira-de-jogos.sj.ifsc.edu.br/api/v1/extrato', {
+    id: username,
+    senha: password
+  })
+    .then(response => {
+      console.log('Resposta do servidor:', response.data)
+    })
+    .catch(error => {
+      console.error('Erro na solicitação:', error)
+    })
+})
